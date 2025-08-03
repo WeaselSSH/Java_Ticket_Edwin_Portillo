d42953e9b11a2f77dd7c27d03b070102247ff185
Edwin Portillo <weaselssh@gmail.com>
2025-08-02 19:34:06 -0600

    probablemente getrol no sea necesario

--- src/java_ticket_edwin_portillo/ManejoEventos.java
+++ src/java_ticket_edwin_portillo/ManejoEventos.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 public class ManejoEventos {
+
     private ArrayList<Evento> eventos;
-    
+
     public ManejoEventos() {
         eventos = new ArrayList<>();
     }
-    
-    public void crearEventoReligioso(Usuario usuario, String codigo,  String titulo, String descripcion, 
-                            Calendar fechaRealizar, double montoRenta) {
-        
+
+    public void crearEventoReligioso(Usuario usuario, String codigo, String titulo, String descripcion,
+            Calendar fechaRealizar, double montoRenta) {
+
         eventos.add(new EventoReligioso(codigo, titulo, descripcion, fechaRealizar, montoRenta));
-        
+
+        if (usuario instanceof Administrador) {
+            Administrador usuarioAdministrador = (Administrador) usuario;
+            usuarioAdministrador.agregarEvento(codigo);
+        }
+
+        if (usuario instanceof Contenido) {
+            Contenido usuarioAdministrador = (Contenido) usuario;
+            usuarioAdministrador.agregarEvento(codigo);
+        }
     }
 }
